4da60ad816b8f1481e7dbb4d0941721df4fc6456
Temmiie <twang725@gmail.com>
2026-05-07 01:35:14 +1000

    init commitv1

--- app.ts
+++ app.ts
@@ -1,2 +1,13 @@
 import express from 'express';
 
+const app = express();
+const PORT = "3000";
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  console.log("Response sent");
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
