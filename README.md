da7875bca16fc3d240df4fb848994500f3ea1bd2
Temmiie <twang725@gmail.com>
2026-05-07 16:15:33 +1000

    update and added new endpoints and testcases

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Mobile Apps CMS Project Backend
 - [x] Map out elements and endpoints that the client application will need to receive and handle.
 - [ ] Construct a simple backend application that can handle some of these endpoints that as a prototype.
 - [ ] Create some simple test cases that will test the functions. In addition, create test cases that will simulate a client sending data to particular endpoints you have designed.
+
+### Test Cases - Commands
+
+http://localhost:3000/users
+
+curl -X POST http://localhost:3000/users/create \
+     -H "Content-Type: application/json" \
+     -d '{"name": "John"}'
+
+curl -X DELETE http://localhost:3000/users/2
+
--- app.ts
+++ app.ts
@@ -1,11 +1,50 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
 const PORT = "3000";
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Overclock Media Backend!");
   console.log("Response sent");
+});
+
+app.use(express.json());
+
+const users = [
+  { "id" : "1","name" : "Tim" },
+  { "id" : "2","name" : "Tom" },
+  { "id" : "3","name" : "Tem" },
+]
+
+app.get("/users", (req, res)=> {
+  res.send(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const {id} = req.params;
+  const obj = users.find((u) => u.id === id);
+  if (!obj){
+    return res.status(404).send("not found");
+  }
+  res.send(obj);
+});
+
+app.post("/users/create", (req, res) => {
+  const { name } = req.body;
+  const id = parseInt(users[users.length-1].id) + 1;
+  const obj = {"id" : id.toString(), "name" : name };
+  users.push(obj);
+  res.send(obj);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const {id} = req.params;
+  const index = users.findIndex((u) => u.id === id);
+  if (index < 0){
+    return res.status(404).send("not found");
+  }
+  users.splice(index,1);
+  res.send("Deleted id:" + id);  
 });
 
 app.listen(PORT, () => {
